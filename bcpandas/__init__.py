3be19d2f664847cb4fc9ea9be308babf2781b9da
TPeter2 <tpeter@greenmountaintechnology.com>
2023-06-28 09:42:28 -0500

    changed package name

--- bcpandas/__init__.py
+++ bcpandas/__init__.py
@@ -6,7 +6,7 @@ import pkg_resources
 from bcpandas.main import SqlCreds, to_sql
 from bcpandas.utils import bcp
 
-name = "bcpandas"
+name = "bcpandas_git"
 try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except Exception:
